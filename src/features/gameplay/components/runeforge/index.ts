90716eae0da5dffcfd1006e6ce30fb0d1bde19ef
Vlad <vlad92z@users.noreply.github.com>
2025-11-23 09:24:17 +0000

    Remove unused folders

--- src/features/gameplay/components/runeforge/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Barrel exports for runeforge-related components
- */
-
-export * from '../Runeforge';
-export * from '../RuneforgesAndCenter';
-export * from '../CenterPool';
-export * from '../RunePower';
--- src/features/gameplay/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Barrel exports for small gameplay UI components
- */
-
-export * from '../PlayerView';
-export * from '../PlayerStats';
-export * from '../SpellpowerExplanation';
