7335d4186849cfbb0998dbf833844f8ae24c68f4
Vlad <vlad92z@users.noreply.github.com>
2025-12-02 09:04:21 +0000

    Delete old files

--- src/features/gameplay/components/GameBoard.tsx
+++
@@ -1,8 +0,0 @@
-/**
- * GameBoard exports - convenience re-exports for solo and duel boards
- */
-
-export { GameBoardFrame } from './GameBoardFrame';
-export { SoloGameBoard } from './SoloGameBoard';
-export { DuelGameBoard } from './DuelGameBoard';
-export { DuelGameBoard as GameBoard } from './DuelGameBoard';
